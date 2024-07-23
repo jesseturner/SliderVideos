b93c2a6ae7b952854fc88bed65f6403b14f32c33
Jesse Turner <jturner@gennai.local>
2024-07-23 13:30:26 -0600

    Added slider setup

--- images_to_video.py
+++ images_to_video.py
@@ -1,3 +1,6 @@
+#--- Slider imagery link: 
+#------ https://rammb-slider.cira.colostate.edu/?sat=goes-16&sec=full_disk&x=12564&y=3100&z=3&angle=0&im=12&ts=6&st=20240709000020&et=20240715000021&speed=140&motion=loop&maps%5Bborders%5D=white&p%5B0%5D=cloud_mask_cira_clavr-x&opacity%5B0%5D=0.96&pause=20240713051021&slider=-1&hide_controls=0&mouse_draw=0&follow_feature=0&follow_hide=0&s=rammb-slider&draw_color=FFD700&draw_width=6
+
 from moviepy.editor import ImageSequenceClip
 
 # Path to the folder containing the .png images
